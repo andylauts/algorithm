a5acb64cbddb4627ffc8679b9eb89892d2ddca75
andyliucn2020 <andyliucn2020@gmail.com>
2019-06-21 19:24:18 +0800

    sort2待处理

--- src/main/java/com/andyliu/algorithm/sorts/InsertSort.java
+++ src/main/java/com/andyliu/algorithm/sorts/InsertSort.java
@@ -9,6 +9,12 @@ import java.util.List;
  * 插入排序
  */
 public class InsertSort {
+    /**
+     * 这个方法自己造的 貌似有点问题 照课本写一个 再对照吧
+     *
+     * @param l
+     * @return
+     */
     public static List<Integer> selectSort(List<Integer> l) {
 
         for (int i = 0; i < l.size(); i++) {
@@ -25,6 +31,19 @@ public class InsertSort {
     private static void insert(List<Integer> l, int i, int j) {
         l.add(i, l.get(j));
         l.remove(j + 1);
+    }
+
+    public static List<Integer> selectSortBook(List<Integer> l) {
+
+        for (int i = 1; i < l.size(); i++) {
+            //处理的第i个位置
+            int k = i;
+            for (int j = i+1; j > 0; j--)
+                if (l.get(i) > l.get(j)) {
+                    insert(l, i, j);
+                }
+        }
+        return l;
     }
 
     public static void main(String[] args) {
