a7cb0b3419e2ded460d93b8d3067281c43b06fcc
andyliucn2020 <andyliucn2020@gmail.com>
2019-06-25 21:59:50 +0800

    shellsort排序 半成品、

--- src/main/java/com/andyliu/algorithm/sorts/ShellSort.java
+++ src/main/java/com/andyliu/algorithm/sorts/ShellSort.java
@@ -2,7 +2,7 @@ package com.andyliu.algorithm.sorts;
 
 public class ShellSort {
 
-    public static int[] shellsort(int[] array) {
+    public static int[] shellsortbook(int[] array) {
         int len = array.length;
         int temp, gap = len / 2;
         while (gap > 0) {
@@ -10,20 +10,48 @@ public class ShellSort {
                 temp = array[i];
                 int preIndex = i - gap;
                 while (preIndex >= 0 && array[preIndex] > temp) {
-                    array[preIndex+gap] = array[preIndex];
+                    array[preIndex + gap] = array[preIndex];
                     preIndex -= gap;
                 }
-                array[preIndex+gap] = temp;
+                array[preIndex + gap] = temp;
 
             }
-            gap/=2;
+            gap /= 2;
         }
+        return array;
+    }
+
+    public static int[] shellsorthand(int[] array) {
+        int len = array.length;
+        int temp = len / 2;
+        for (int gap = len / 2; gap > 0; gap /= 2)
+            for (int i = 0; i < gap; i++) {
+                int first = i;
+                int min = array[i];
+                int minindex = i;
+                for (int j = i + gap; j < len; j += gap) {
+                    if (array[j] > array[j-gap]){
+                        min = array[j];
+                        minindex = j;
+                    }
+                    if(i!=minindex){
+                        int changetmp = array[i];
+                        int firstint = array[minindex];
+                        array[i] = array[minindex];
+
+
+                    }
+
+                    array[j] = array[i];
+                    array[i] = temp;
+                }
+            }
         return array;
     }
 
     public static void main(String[] args) {
         System.out.println("alksdjfalkdsjf");
-        for (int x : shellsort(new int[]{23, 423, 3, 234, 235, 23, 3}))
+        for (int x : shellsorthand(new int[]{23, 423, 3, 234, 235, 23, 3}))
             System.out.println(x);
         System.out.println("alksdjfalkdsjf");
     }
